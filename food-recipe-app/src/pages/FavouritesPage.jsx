4ac723c6daf6160c13cf75f0ac9e5e2cc814decf
eliao13 <101660088+eliao13@users.noreply.github.com>
2024-05-06 17:05:58 -0700

    populate favourites page with local storage favourite recipes

--- food-recipe-app/src/pages/FavouritesPage.jsx
+++ food-recipe-app/src/pages/FavouritesPage.jsx
@@ -1,3 +1,19 @@
+import { useEffect, useState } from "react";
+import Recipe from "../components/home-page-recipe/Recipe";
+
 export default function FavouritesPage() {
-  return <div>Favourites</div>;
+  const [favourites, setFavourites] = useState([]);
+
+  useEffect(() => {
+    const savedRecipes = JSON.parse(localStorage.getItem("recipes")) || [];
+    setFavourites(savedRecipes);
+  }, []);
+
+  return (
+    <div>
+      {favourites.map((recipe) => {
+        return <Recipe key={recipe.id} recipeData={recipe} />;
+      })}
+    </div>
+  );
 }
