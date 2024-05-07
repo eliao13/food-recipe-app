9e9ee18dd4a7a62314813d6470d6fe66c9aba58c
eliao13 <101660088+eliao13@users.noreply.github.com>
2024-05-06 17:05:42 -0700

    Add functionality to add or remove recipe from favourites (local storage)

--- food-recipe-app/src/pages/RecipePage.jsx
+++ food-recipe-app/src/pages/RecipePage.jsx
@@ -5,13 +5,37 @@ import { Button } from "@mui/material";
 
 export default function RecipePage() {
   const [recipeData, setRecipeData] = useState({});
+  const [addedToFavourites, setAddedToFavourites] = useState(false);
   const { id } = useParams();
 
   useEffect(() => {
     fetchRecipe(id).then((data) => {
       setRecipeData(data.data.recipe);
     });
+    checkIfAddedFavourite();
   }, [id]);
+
+  function handleAddFavourite() {
+    setAddedToFavourites(true);
+    const savedRecipes = JSON.parse(localStorage.getItem("recipes")) || [];
+    const updatedRecipes = [...savedRecipes, recipeData];
+    localStorage.setItem("recipes", JSON.stringify(updatedRecipes));
+  }
+
+  function handleRemoveFavourite() {
+    setAddedToFavourites(false);
+    const savedRecipes = JSON.parse(localStorage.getItem("recipes")) || [];
+    const updatedRecipes = savedRecipes.filter((recipe) => recipe.id !== id);
+    localStorage.setItem("recipes", JSON.stringify(updatedRecipes));
+  }
+
+  function checkIfAddedFavourite() {
+    const savedRecipes = JSON.parse(localStorage.getItem("recipes")) || [];
+    const foundRecipe = savedRecipes.find((recipe) => recipe.id === id);
+    if (foundRecipe) {
+      setAddedToFavourites(true);
+    }
+  }
 
   return (
     <section className="recipe">
@@ -21,9 +45,23 @@ export default function RecipePage() {
       <article>
         <h3>{recipeData.publisher}</h3>
         <h1>{recipeData.title}</h1>
-        <Button variant="contained" color="primary">
-          Add to favourites
-        </Button>
+        {addedToFavourites ? (
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleRemoveFavourite}
+          >
+            Remove from favourites
+          </Button>
+        ) : (
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleAddFavourite}
+          >
+            Add to favourites
+          </Button>
+        )}
         <h2>Ingredients:</h2>
         <ul>
           {recipeData.ingredients
